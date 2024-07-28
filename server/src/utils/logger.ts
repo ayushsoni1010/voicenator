c92d3a1009189ea2eb148576ade0fe5c3018c529
Ayush Soni <ayushsoni1010.work@gmail.com>
2024-07-28 20:36:46 +0530

    :sparkles: feat: crafted logger utility

---
+++ server/src/utils/logger.ts
@@ -0,0 +1,18 @@
+import { createLogger, transports, format } from "winston";
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
+
+export default logger;
