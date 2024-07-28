b1e38483cb46ce355bb26090760d14810da7a1a6
Ayush Soni <ayushsoni1010.work@gmail.com>
2024-07-28 22:39:33 +0530

    :art: update: basic real-time transcription app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,105 @@
-import Controls from "@/components/Controls";
+import { useEffect, useState } from "react";
+import useAudioRecorder from "./hooks/useAudioRecorder";
+import useSocket from "./hooks/useSocket";
 
 function App() {
+  const {
+    socket,
+    initialize,
+    handleStartTranscription,
+    disconnect,
+    initializeStream,
+    sendStreamedAudio,
+    transcription,
+  } = useSocket();
+
+  const [isTranscribing, setIsTranscribing] = useState<boolean>(false);
+  const [data, setData] = useState<Int16Array | null>(null);
+
+  useEffect(() => {
+    if (socket === null) {
+      initialize();
+    }
+
+    return () => {
+      disconnect();
+    };
+  }, [initialize, disconnect, socket]);
+
+  useEffect(() => {
+    if (data) {
+      sendStreamedAudio(data);
+    }
+  }, [data, sendStreamedAudio]);
+
+  const {
+    startRecording,
+    stopRecording,
+    isRecording,
+    togglePauseResume,
+    isPaused,
+  } = useAudioRecorder({
+    dataCb: (data: Int16Array) => {
+      setData(data);
+    },
+  });
+
+  const onStartRecordingPress = async () => {
+    try {
+      const sampleRate = await startRecording();
+      initializeStream(sampleRate);
+      handleStartTranscription();
+      setIsTranscribing(true);
+    } catch (error) {
+      console.log("Recording failed");
+    }
+  };
+
+  const onStopRecordingPress = () => {
+    stopRecording();
+    setTimeout(() => {
+      setIsTranscribing(false);
+    }, 3000);
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(transcription);
+  };
+
+  const handleClearText = () => {
+    setData(null);
+  };
+
   return (
-    <>
-      <h1>Vite + React</h1>
-      <Controls />
-    </>
+    <div>
+      <h1>Speechify Voice Notes</h1>
+      <p>Record or type something in the textbox.</p>
+      <textarea
+        id="transcription-display"
+        rows={10}
+        cols={50}
+        value={transcription}
+        readOnly
+      ></textarea>
+      {!isRecording ? (
+        <button id="record-button" onClick={onStartRecordingPress}>
+          {!isTranscribing ? "Start Recording" : "Loading..."}
+        </button>
+      ) : (
+        <button id="record-button" onClick={onStopRecordingPress}>
+          {!isTranscribing ? "Stop Recording" : "Loading..."}
+        </button>
+      )}
+      <button onClick={togglePauseResume}>
+        {isPaused ? "Resume" : "Pause"}
+      </button>
+      <button id="copy-button" onClick={handleCopyClick}>
+        Copy
+      </button>
+      <button id="reset-button" onClick={handleClearText}>
+        Clear
+      </button>
+    </div>
   );
 }
 
